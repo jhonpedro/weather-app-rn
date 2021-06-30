2f85c95c38f21273baa79668deef24332f48bfb1
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-06-30 11:57:27 -0300

    feat: create input wich avoids view component

---
+++ src/components/InputAvoidView.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+	KeyboardAvoidingView,
+	Platform,
+	StyleSheet,
+	Text,
+	TextInput,
+} from 'react-native'
+
+import colors from '../colors/constants'
+
+function InputAvoidView() {
+	return (
+		<KeyboardAvoidingView
+			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+		>
+			<Text style={styles.label}>Type your location here:</Text>
+			<TextInput style={styles.input} />
+		</KeyboardAvoidingView>
+	)
+}
+
+const styles = StyleSheet.create({
+	label: {
+		fontSize: 19,
+		marginBottom: 10,
+	},
+	input: {
+		padding: 10,
+		borderColor: colors.BORDER,
+		borderWidth: 1,
+		borderRadius: 10,
+		fontSize: 16,
+	},
+})
+
+export default InputAvoidView
