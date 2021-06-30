0ace6ecb4a6393ea2419dbe78d676aa1933ee712
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-06-30 16:33:34 -0300

    feat: add react redux provider and its store

--- App.jsx
+++ App.jsx
@@ -2,16 +2,20 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { RootSiblingParent } from 'react-native-root-siblings'
 import { NavigationContainer } from '@react-navigation/native'
+import { Provider } from 'react-redux'
 
 import Routes from './src/routes'
+import store from './src/store'
 
 export default function App() {
 	return (
 		<RootSiblingParent>
-			<NavigationContainer>
-				<Routes />
-				<StatusBar style='auto' />
-			</NavigationContainer>
+			<Provider store={store}>
+				<NavigationContainer>
+					<Routes />
+					<StatusBar style='auto' />
+				</NavigationContainer>
+			</Provider>
 		</RootSiblingParent>
 	)
 }
