ed1e84400d36bfe1a0bc6a2761d433a04c321bfc
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-01 10:53:28 -0300

    feat: listen to changes in searches for request

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react'
 import { WHEATHER_API_KEY } from '@env'
 import { apiResponse } from '../services/apiResponse'
+import { useDispatch } from 'react-redux'
+import { actionRemoveRequest } from '../store/reducers/search/actions'
+import useSearch from '../store/selectors/useSearch'
 
 const initial_value = apiResponse
 
 const useWeather = ({ callback, currentUnitSystem }) => {
+	const searchs = useSearch()
 	const [loading, setLoading] = useState(true)
 	const [weather, setWeather] = useState(initial_value)
 	const [reexecute, setReexecute] = useState(0)
+	const dispatch = useDispatch()
 
 	useEffect(() => {
 		setLoading(true)
@@ -16,16 +21,21 @@ const useWeather = ({ callback, currentUnitSystem }) => {
 				return
 			}
 
-			const { latitude, longitude } = locationData.coords
+			const { latitude, longitude } = searchs.makeRequest
+				? { latitude: searchs.items[0].lat, longitude: searchs.items[0].lng }
+				: locationData.coords
 
 			const requestURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${WHEATHER_API_KEY}&units=${currentUnitSystem}`
 
 			fetch(requestURL)
 				.then((response) => response.json())
 				.then((data) => setWeather(data))
-				.finally(() => setLoading(false))
+				.finally(() => {
+					setLoading(false)
+					dispatch(actionRemoveRequest())
+				})
 		})
-	}, [reexecute, currentUnitSystem])
+	}, [searchs.items, reexecute, currentUnitSystem])
 
 	const reload = () => {
 		setReexecute((prevState) => prevState + 1)
